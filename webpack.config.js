c3395ad92c2bb946d30b7d8a10e01b53be77b0f1
ebrocato <emilybrocato@outlook.com>
2019-05-13 11:22:48 -0600

    updates

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/assets/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index_bundle.js'
+    },
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/assets/index.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
